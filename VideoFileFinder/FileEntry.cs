d33a0685909fe6a0b8e2c2a67d3646e7d52614b3
Anakin <anakin@gmx-topmail.de>
2019-12-11 19:28:34 +0100

    fix deleting wrong entry

---
+++ VideoFileFinder/FileEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Media;
+using Microsoft.WindowsAPICodePack.Shell;
+
+namespace VideoFileFinder
+{
+    public struct FileEntry : IEquatable<FileEntry>
+    {
+        public string FilePath { get; }
+
+        public ImageSource Thumbnail { get; }
+
+        public string Tags { get; }
+
+        public FileEntry(string path)
+        {
+            FilePath = path;
+            var file = ShellFile.FromFilePath(path);
+            Thumbnail = file.Thumbnail.BitmapSource;
+            var tags = file.Properties.System.Keywords.Value;
+            Tags = tags == null ? string.Empty : string.Join("; ", file.Properties.System.Keywords.Value);
+        }
+
+        public bool Equals(FileEntry other)
+        {
+            return FilePath == other.FilePath;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is FileEntry other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return (FilePath != null ? FilePath.GetHashCode() : 0);
+        }
+    }
+}
--- VideoFileFinder/RandomEntry.cs
+++ VideoFileFinder/RandomEntry.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Media;
-using Microsoft.WindowsAPICodePack.Shell;
 
 namespace VideoFileFinder
 {
@@ -21,12 +19,13 @@ namespace VideoFileFinder
             Filter = filter;
             Drives = drives;
             LogicalOr = logicalOr;
-            Files = files.Select(x => new FileEntry(x)).ToList();
+            Files = files.Select(x => new FileEntry(x)).Distinct();
         }
 
         public bool Equals(RandomEntry other)
         {
-            return string.Equals(Filter, other.Filter) && string.Equals(Drives, other.Drives) && LogicalOr == other.LogicalOr;
+            return string.Equals(Filter, other.Filter) && string.Equals(Drives, other.Drives) &&
+                   LogicalOr == other.LogicalOr && Files.Equals(other.Files);
         }
 
         public override bool Equals(object obj)
@@ -43,24 +42,6 @@ namespace VideoFileFinder
                 hashCode = (hashCode * 397) ^ LogicalOr.GetHashCode();
                 return hashCode;
             }
-        }
-    }
-
-    public struct FileEntry
-    {
-        public string FilePath { get; }
-
-        public ImageSource Thumbnail { get; }
-
-        public string Tags { get; }
-
-        public FileEntry(string path)
-        {
-            FilePath = path;
-            var file = ShellFile.FromFilePath(path);
-            Thumbnail = file.Thumbnail.BitmapSource;
-            var tags = file.Properties.System.Keywords.Value;
-            Tags = tags == null ? string.Empty : string.Join("; ", file.Properties.System.Keywords.Value);
         }
     }
 }
